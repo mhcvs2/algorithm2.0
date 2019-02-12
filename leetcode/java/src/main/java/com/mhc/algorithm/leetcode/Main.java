7b10f7bb871c5f9c74171e105a0815e1b2d18181
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2019-02-12 14:12:11 +0800

    add

---
+++ leetcode/java/src/main/java/com/mhc/algorithm/leetcode/Main.java
@@ -0,0 +1,20 @@
+package com.mhc.algorithm.leetcode;
+
+/*
+
+*/
+
+class Solution {
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        System.out.println();
+    }
+
+}
